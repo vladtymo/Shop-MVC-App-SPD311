aa17974fc438f5a3e9edbf55702ab9e8c7c9e08e
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2025-02-22 17:12:31 +0200

    Scaffold teams views and controller

---
+++ MVC_App_SPD311/Controllers/TeamsController.cs
@@ -0,0 +1,114 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVC_App_SPD311.Data;
+using MVC_App_SPD311.Models;
+
+namespace MVC_App_SPD311.Controllers
+{
+    public class TeamsController : Controller
+    {
+        private readonly FootballDbContext _context;
+
+        public TeamsController()
+        {
+            _context = new FootballDbContext();
+        }
+
+        // GET: Teams
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.FootballTeams.ToListAsync());
+        }
+
+        // GET: Teams/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var team = await _context.FootballTeams.FindAsync(id);
+            
+            if (team == null) return NotFound();
+            
+            return View(team);
+        }
+
+        // GET: Teams/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Teams/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Logo,Name,Players,Country")] Team team)
+        {
+            if (!ModelState.IsValid) return View(team);
+            
+            _context.Add(team);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Teams/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var team = await _context.FootballTeams.FindAsync(id);
+            if (team == null) return NotFound();
+            
+            return View(team);
+        }
+
+        // POST: Teams/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Logo,Name,Players,Country")] Team team)
+        {
+            if (id != team.Id) return NotFound();
+
+            if (!ModelState.IsValid) return View(team);
+            
+            _context.Update(team);
+            await _context.SaveChangesAsync();
+            
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Teams/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var team = await _context.FootballTeams.FindAsync(id);
+            
+            if (team == null) return NotFound();
+
+            return View(team);
+        }
+
+        // POST: Teams/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var team = await _context.FootballTeams.FindAsync(id);
+            if (team == null) return NoContent();
+            
+            _context.FootballTeams.Remove(team);
+            await _context.SaveChangesAsync();
+            
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TeamExists(int id)
+        {
+            return _context.FootballTeams.Any(e => e.Id == id);
+        }
+    }
+}
